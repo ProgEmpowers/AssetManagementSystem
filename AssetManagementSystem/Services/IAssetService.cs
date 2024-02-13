e9fb66e68208a1d660d9a0b3b1b96e807bfff0f1
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-02-13 11:07:00 +0530

    AM-003-BE-Update-Assets-in-the-DB-- task done

--- AssetManagementSystem/Services/IAssetService.cs
+++ AssetManagementSystem/Services/IAssetService.cs
@@ -8,5 +8,6 @@ namespace AssetManagementSystem.Services
         Task<Asset> AddAssetAsync(AssetDto newAsset);
         Task<List<Asset>> GetAllAssetsAsync();
         Task<Asset?> GetAssetByIdAsync(int id);
+        Task<Asset?> UpdateAssetAsync(int id, AssetDto assetDto);
     }
 }
