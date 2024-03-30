e1c0819d207c8ac18b20a11b8c288983bfa7d12b
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-03-30 06:10:39 +0530

    Created API-Gateway

--- AssetManagementSystem/Models/Dtos/AssetDto.cs
+++ AssetManagementSystem/Models/Dtos/AssetDto.cs
@@ -9,8 +9,8 @@ namespace AssetManagementSystem.Models.Dtos
         [MaxLength(5, ErrorMessage = "max")]
         [MinLength(3, ErrorMessage = "min")]
         public string Name { get; set; }
-        public string AssetType { get; set; }
-        public string Description { get; set; }
+        public string? AssetType { get; set; }
+        public string? Description { get; set; }
         public string? ImageUrl { get; set; }
         public string? QRcode { get; set; }
         public float? AssetValue { get; set; }
